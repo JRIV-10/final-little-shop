ed6725267e79aa9c53c5904f6bd118b549f8c105
Joey Reyes <joeyreyes105@gmail.com>
2024-03-02 11:36:13 -0800

    Fix all error codes and finish us-5, all tests pass

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -15,12 +15,12 @@ class Merchant::BulkDiscountsController < ApplicationController
 
   def create 
     @merchant = Merchant.find(params[:merchant_id])
-    bulk_discount = BulkDiscount.new(bulk_discount_params)
-    if bulk_discount.save 
+    @merchant.bulk_discounts.new(bulk_discount_params)
+    if @merchant.save 
       redirect_to merchant_bulk_discounts_path(@merchant)
       flash[:notice] = "Succesfully Created a New Discount"
     else 
-      require 'pry'; binding.pry
+      # require 'pry'; binding.pry
       redirect_to new_merchant_bulk_discount_path(@merchant)
       flash[:alert] = "Please fill in ALL required fields"
     end
@@ -35,13 +35,13 @@ class Merchant::BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.find(params[:id])
     if @bulk_discount.update(bulk_discount_params)
-      flash[:notice] = "Congratulations! You've edited the discount successfully!"
-
       redirect_to merchant_bulk_discount_path(params[:merchant_id], params[:id])
-    else
-      flash.now[:error] = "Couldn't fully update the Discount, please make sure ALL fields are filled out properly"
 
-      render :edit
+      flash[:notice] = "Congratulations! You've edited the discount successfully!"
+    else
+      redirect_to edit_merchant_bulk_discount_path(params[:merchant_id], params[:id])
+
+      flash[:alert] = "Couldn't fully update the Discount, please make sure ALL fields are filled out properly"
     end 
   end
 
--- spec/features/bulk_discounts/edit_spec.rb
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'Edit Page', type: :feature do
       @discount_1 = @merchant_1.bulk_discounts.create!(discount: 10, quantity: 5)
       @discount_2 = @merchant_1.bulk_discounts.create!(discount: 9, quantity: 3)
     end
+
     describe "US-5" do
       it 'can edit the discount information' do
         visit edit_merchant_bulk_discount_path(@merchant_1, @discount_1)
         # And I see that the discounts current attributes are pre-poluated in the form
-        # save_and_open_page
         expect(page.find_field("Discount").value).to have_content("10")
         expect(page.find_field("Quantity").value).to have_content("5")
         
@@ -24,6 +24,16 @@ RSpec.describe 'Edit Page', type: :feature do
         # And I see that the discount's attributes have been updated
         expect(page).to have_content("20")
         expect(page).to have_content("10")
+      end
+
+      it "has an error message if not all fields are filled out" do 
+        visit edit_merchant_bulk_discount_path(@merchant_1, @discount_1)
+        fill_in("Discount", with: 20)
+        fill_in("Quantity", with: '')
+        click_on("Update Bulk discount")
+
+        expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant_1, @discount_1))
+        expect(page).to have_content("Couldn't fully update the Discount, please make sure ALL fields are filled out properly")
       end
     end
   end
